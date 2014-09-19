c76dc0be3eef4dce68fd13f129eb8151453a2e77
philipMannheimer <philip.mannheimer@gmail.com>
2014-09-19 11:08:19 +0200

    starting with generator to copy asset

--- lib/my_javascript_library/engine.rb
+++ lib/my_javascript_library/engine.rb
@@ -1,6 +1,5 @@
 module MyJavascriptLibrary
   class Engine < ::Rails::Engine
-  	isolate_namespace MyJavascriptLibrary::Rails
     initializer 'my_javascript_library.load_my_javascript_library' do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
     end
