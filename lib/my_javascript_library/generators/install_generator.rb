13bdf65553e47ae1ec8a7b0f0e793cc9b3fd351c
philipMannheimer <philip.mannheimer@gmail.com>
2014-09-19 11:10:09 +0200

    starting with generator to copy asset

--- lib/my_javascript_library/generators/install_generator.rb
+++ lib/my_javascript_library/generators/install_generator.rb
@@ -4,7 +4,7 @@ module MyJavascriptLibrary
 	module Generators
 		class InstallGenerator < ::Rails::Generators::Base
 			def add_assets
-				base_functions = File.join(File.dirname(__FILE__),"..", "..", "..", "..", "vendor", "assets", "javascripts", "base-functions.coffee")
+				base_functions = File.join(File.dirname(__FILE__),"..", "..", "..", "vendor", "assets", "javascripts", "base-functions.coffee")
         create_file "app/assets/javascripts/base-functions.coffee", File.read(base_functions)
 			end
 		end
